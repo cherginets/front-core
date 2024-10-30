94d5d89417072cc21a992bf8609f925fe740e673
Anton Cherginets <anton.cherginets@gmail.com>
2024-10-30 15:38:27 +0300

    fixes

---
+++ components/CutBlock.tsx
@@ -0,0 +1,21 @@
+import {ReactNode, useState} from "react";
+import {Button, Stack} from "@mui/material";
+import {ChevronLeft} from "@mui/icons-material";
+
+export default function CutBlock(
+  {
+                                   showMessage = "Показать",
+                                   hideMessage = "Скрыть",
+                                   children,
+                                 }: {
+  showMessage?: string,
+  hideMessage?: string,
+  children?: ReactNode
+}) {
+  const [open, setOpen] = useState(false);
+
+  return <Stack direction={'column'} spacing={1}>
+    <Button onClick={() => setOpen(o => !o)}>{open ? hideMessage : showMessage}</Button>
+    {open && <div style={{width: '100%'}}>{children}</div>}
+  </Stack>
+}
