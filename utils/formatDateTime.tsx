4748c08fd75e2b3ae3f4e1591ff5472e5d00a803
Anton Cherginets <anton.cherginets@gmail.com>
2024-11-16 16:34:05 +0300

    stable

--- utils/formatDateTime.tsx
+++ utils/formatDateTime.tsx
@@ -1,6 +1,6 @@
-import {MOMENT_DATE_MYSQL} from "@/core/utils/formatters";
+import {MOMENT_DATE_MYSQL, MOMENT_DATE_PRETTY} from "@/core/utils/formatters";
 import moment from "moment";
 
 export default function formatDateTime(str: string) {
-  return moment(str).format(MOMENT_DATE_MYSQL);
+  return moment(str).format(MOMENT_DATE_PRETTY);
 }
