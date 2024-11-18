9771ed9a9b1d76343dd890348d03cf7e335f359f
Anton Cherginets <anton.cherginets@gmail.com>
2024-11-18 17:01:08 +0300

    tgstat progress

--- components/DetailsTable.tsx
+++ components/DetailsTable.tsx
@@ -49,7 +49,7 @@ const DetailsTable: FC<DetailsTableProps> = ({ groups }) => {
                     <TableCell variant={"head"} align={"right"}>
                       <Typography
                         variant={"subtitle2"}
-                        style={{ whiteSpace: "nowrap" }}
+                        style={{ maxWidth: 250, }}
                       >
                         {row.title}
                       </Typography>
