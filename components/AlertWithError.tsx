5d869daf25d4bf5b572361c544a31824e08849c7
Anton Cherginets <anton.cherginets@gmail.com>
2024-11-20 10:09:28 +0300

    admin orders crud init

--- components/AlertWithError.tsx
+++ components/AlertWithError.tsx
@@ -1,14 +1,18 @@
 import { formatError } from "@/core/utils/formatError";
 import Alert, { AlertProps } from "@mui/material/Alert";
+import Button from "@mui/material/Button";
 
 export function AlertWithError({
   error,
+  refetch,
   ...props
-}: { error: any } & AlertProps) {
+}: { error: any, refetch?: () => any } & AlertProps) {
   if (!error) return null;
   return (
     <Alert severity={"error"} {...props}>
       <pre style={{ whiteSpace: "break-spaces" }}>{formatError(error)}</pre>
+      {refetch && <Button variant={'contained'} style={{marginTop: 8}} onClick={() => refetch()}>Перезагрузить</Button>}
+
     </Alert>
   );
 }
