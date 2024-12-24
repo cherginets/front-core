24e6dbac9f6fcb561686ee9a5d85c6fbec24c9fd
Anton Cherginets <anton.cherginets@gmail.com>
2024-12-24 14:28:18 +0300

    progress

---
+++ utils/files.ts
@@ -0,0 +1,13 @@
+export function download(filename: string, text: string): void {
+  var element = document.createElement('a');
+  var universalBOM = "\uFEFF";
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(universalBOM + text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
