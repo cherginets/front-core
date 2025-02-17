dad9c835617406d05d568bfe3a82d517cac4ce95
Anton Cherginets <anton.cherginets@gmail.com>
2025-02-17 17:24:04 +0300

    redirects

---
+++ next/middlewares/redirectsMiddleware.ts
@@ -0,0 +1,22 @@
+import {NextRequest, NextResponse} from "next/server";
+import {APP_HOST} from "@/core/constants";
+
+export type Redirects = {
+  [key: string]: {
+    destination: string;
+    permanent: boolean;
+  }
+}
+export const redirectsMiddleware = (
+  req: NextRequest,
+  {data}: {
+    data: Redirects,
+  },
+  callback: (req: NextRequest) => any
+) => {
+  if (req.nextUrl.pathname in data) {
+    return NextResponse.redirect(APP_HOST + data[req.nextUrl.pathname].destination, 301)
+  }
+
+  return callback(req);
+};
