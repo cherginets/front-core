9a9df0abb976b8ecfb17fe80986085f7678923b3
Anton Cherginets <anton.cherginets@gmail.com>
2025-02-25 18:07:55 +0300

    aggregateArray

---
+++ utils/aggregateArray.ts
@@ -0,0 +1,20 @@
+/**
+ *
+ * @param arr массив для агрегации
+ * @param getAggregatedValue должна отдавать ключ, по которому агрегируем массив
+ */
+export const aggregateArray = function <T extends any, V>(
+  arr: T[],
+  getAggregatedValue: (row: T) => V
+) {
+  const map = new Map<V, T[]>();
+
+  for(const item of arr) {
+    const key = getAggregatedValue(item);
+    if(!map.has(key)) map.set(key, []);
+
+    map.get(key)!.push(item);
+  }
+
+  return map;
+}
