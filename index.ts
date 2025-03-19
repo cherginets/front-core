6b3c97f807602ebde5e19497e338d4a5b4592256
Anton Cherginets <anton.cherginets@gmail.com>
2025-03-19 16:13:52 +0300

    minAbs & maxAbs & orders fixed

--- index.ts
+++ index.ts
@@ -3,4 +3,27 @@ import {ru} from "yup-locales";
 
 yup.setLocale(ru);
 
+declare module 'yup' {
+  interface NumberSchema {
+    minAbs(minValue: number, message?: string): this;
+    maxAbs(maxValue: number, message?: string): this;
+  }
+}
+
+yup.addMethod(yup.number, 'minAbs', function (minValue, message) {
+  return this.test({
+    name: 'minAbs',
+    message: message || `Значение по модулю должно быть ≥ ${minValue}`,
+    test: value => value !== undefined && Math.abs(value) >= minValue,
+  });
+});
+
+yup.addMethod(yup.number, 'maxAbs', function (maxValue, message) {
+  return this.test({
+    name: 'maxAbs',
+    message: message || `Значение по модулю должно быть ≥ ${maxValue}`,
+    test: value => value !== undefined && Math.abs(value) >= maxValue,
+  });
+});
+
 export {yup};
