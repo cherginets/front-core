6753c6e7a9b135d6f0a3e825463b3c3451e8ff7f
Anton Cherginets <anton.cherginets@gmail.com>
2025-03-24 15:54:17 +0300

    yup locale

---
+++ formik/fields/FormikCheckboxField.tsx
@@ -0,0 +1,33 @@
+import {isNumeric} from "@/core/utils/isNumeric";
+import TextField, {TextFieldProps} from "@mui/material/TextField";
+import {useField, useFormikContext} from "formik";
+import {Checkbox} from "@mui/material";
+import {ReactNode} from "react";
+
+export type FormikCheckboxFieldProps = {
+  name: string
+  label: ReactNode
+};
+const FormikCheckboxField = ({ label, name, ...props }: FormikCheckboxFieldProps) => {
+  const [field, meta, helpers] = useField(name);
+
+  return (
+    <div className={'flex flex-col gap-2'}>
+      <label className="flex items-center gap-1 cursor-pointer">
+        <Checkbox
+          id={name}
+          size={'medium'}
+          checked={field.value}
+          onChange={(event, checked) => helpers.setValue(checked)}
+          {...props}
+        />
+        <div className={'inline text-left text-xs'}>{label}</div>
+
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="text-red-500 text-xs text-left">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+export default FormikCheckboxField;
--- index.ts
+++ index.ts
@@ -1,29 +1,3 @@
-import * as yup from "yup";
-import {ru} from "yup-locales";
-
-yup.setLocale(ru);
-
-declare module 'yup' {
-  interface NumberSchema {
-    minAbs(minValue: number, message?: string): this;
-    maxAbs(maxValue: number, message?: string): this;
-  }
-}
-
-yup.addMethod(yup.number, 'minAbs', function (minValue, message) {
-  return this.test({
-    name: 'minAbs',
-    message: message || `Значение по модулю должно быть ≥ ${minValue}`,
-    test: value => value !== undefined && Math.abs(value) >= minValue,
-  });
-});
-
-yup.addMethod(yup.number, 'maxAbs', function (maxValue, message) {
-  return this.test({
-    name: 'maxAbs',
-    message: message || `Значение по модулю должно быть ≥ ${maxValue}`,
-    test: value => value !== undefined && Math.abs(value) >= maxValue,
-  });
-});
+import yup from "./yup";
 
 export {yup};
