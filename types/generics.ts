455d6849322a65bf1ec84e5050d134796cfcd7a4
Anton Cherginets <anton.cherginets@gmail.com>
2025-04-14 11:48:40 +0300

    build stable

--- types/generics.ts
+++ types/generics.ts
@@ -1,1 +1,2 @@
-export type RequiredOnly<T, Keys extends keyof T> = Required<Pick<T, Keys>> & Omit<T, Keys>;
+// Required<Pick<LkChannel, 'id'>> & Partial<Omit<LkChannel, 'id'>>
+export type RequiredOnly<T, Keys extends keyof T> = Required<Pick<T, Keys>> & Partial<Omit<T, Keys>>;
