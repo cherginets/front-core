0cd97c87d2a36efa6b9cc8181270fd7a3b389d63
Anton <anton.cherginets@gmail.com>
2025-05-30 15:29:13 +0300

    some notifications progress

--- constants.ts
+++ constants.ts
@@ -1,5 +1,7 @@
 export const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL as string;
 
+export const NEXT_PUBLIC_IS_BUILD = !!process.env.NEXT_PUBLIC_IS_BUILD as boolean;
+
 export const IS_PROD = process.env.NODE_ENV === "production";
 export const IS_DEV = process.env.NODE_ENV === "development";
 export const DEBUG_ON = process.env.NEXT_PUBLIC_DEBUG === "on";
