c3497769c68ab8eed40bc584b8a582d3cbcf98e2
Anton <anton.cherginets@gmail.com>
2025-05-31 13:47:36 +0300

    new ws server

--- constants.ts
+++ constants.ts
@@ -7,6 +7,7 @@ export const IS_DEV = process.env.NODE_ENV === "development";
 export const DEBUG_ON = process.env.NEXT_PUBLIC_DEBUG === "on";
 
 export const APP_HOST = (process.env.APP_HOST as string) || (process.env.NEXT_PUBLIC_APP_HOST as string);
+export const SOCKET_HOST = (process.env.NEXT_PUBLIC_SOCKET_HOST as string) || (process.env.NEXT_PUBLIC_APP_HOST as string);
 
 export const CHAR_DOT = "•";
 export const CHAR_RUBLE = "₽";
